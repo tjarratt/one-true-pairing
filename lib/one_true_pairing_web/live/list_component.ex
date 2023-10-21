bcfff5104fad332a7e62460c075c4e591c3ace91
Tim Jarratt <tim.jarratt@maersk.com>
2023-10-21 22:56:07 +0200

    More layout

--- lib/one_true_pairing_web/live/list_component.ex
+++ lib/one_true_pairing_web/live/list_component.ex
@@ -4,10 +4,10 @@ defmodule OneTruePairingWeb.Live.ListComponent do
   def render(assigns) do
     ~H"""
     <div class="bg-gray-100 py-4 rounded-lg" test-role={@test_role}>
-      <div class="space-y-5 mx-auto max-w-7xl px-4 space-y-4">
+      <div class="space-y-5 mx-auto px-4 space-y-4">
         <.header>
           <.simple_form for={@form} phx-change="validate" phx-submit="save" phx-target={@myself}>
-            <.input type="text" test-role="track-name" name={"track-named-#{@list_name}"} value={@list_name} />
+            <.input type="text" test-role="track-name" name={"track-named-#{@list_name}"} value={@list_name} class="text-2xl"/>
           </.simple_form>
         </.header>
 
@@ -15,12 +15,12 @@ defmodule OneTruePairingWeb.Live.ListComponent do
           <div
             :for={item <- @list}
             id={"#{@id}-#{item.id}"}
-            class="drag-item:focus-within:ring-0 drag-item:focus-within:ring-offset-0 drag-ghost:bg-zinc-300 drag-ghost:border-0 drag-ghost:ring-0 m-2 border-1 border-dashed border-slate-50"
+            class="drag-item:focus-within:ring-0 drag-item:focus-within:ring-offset-0 drag-ghost:bg-zinc-300 drag-ghost:border-0 drag-ghost:ring-0 m-2 border-2 border-dashed border-slate-300"
             data-id={item.id}
           >
-            <div class="flex drag-ghost:opacity-0 border-5">
+            <div class="flex drag-ghost:opacity-0">
               <.icon name="hero-user-circle" class="w-7 h-7 bg-gray-300 mr-2" />
-              <div class="flex-auto block text-sm leading-6 text-zinc-900">
+              <div class="flex-auto block text-sm leading-6 text-zinc-900 text-xl">
                 <%= item.name %>
               </div>
             </div>
