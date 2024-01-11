b50ab633282eeb7d847d719ae6e3ace7c1053207
Tim Jarratt <tjarratt@gmail.com>
2024-01-11 19:34:07 +0100

    Create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+project = OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Project{name: "Fellowship"})
+
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Sam", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Gandalf", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Gimli", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Legolas", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Aragorn", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Boromir", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Frodo", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Pippin", project_id: project.id})
+OneTruePairing.Repo.insert!(%OneTruePairing.Projects.Person{name: "Merry", project_id: project.id})
+
