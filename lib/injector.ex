a96ff1c7a949c819fc373d25863d5a2452f2d68b
Tim Jarratt <tjarratt@gmail.com>
2024-03-23 19:52:30 +0100

    Typespecs
    Co-authored-by: Sarah Stoltze <sarah.stoltze@maersk.com>

--- lib/injector.ex
+++ lib/injector.ex
@@ -1,10 +1,12 @@
 defmodule Injector do
+  @spec inject(atom(), module()) :: term() | nil
   def inject(key, stub) do
     Process.put(key, stub)
   end
 end
 
 defmodule Provider do
+  @spec provide(atom(), default: module()) :: module()
   def provide(key, default: default) do
     ProcessTree.get(key, default: default)
   end
