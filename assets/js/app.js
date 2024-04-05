f93fb4c72f0ae23849bbcdfe2adbffdfd29e9486
Alicia Gonzalez <alicia.gonzalez@maersk.com>
2024-04-05 16:47:36 +0200

    Format in js file
    Co-authored-by: Cassio Dias <cassio.dias@maersk.com>
    Co-authored-by: Freja Kelleris <freja.kelleris@maersk.com>
    Co-authored-by: Ronaldo Sousa <ronaldo.lima.de.sousa@maersk.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,8 +19,8 @@
 import "phoenix_html"
 
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import Sortable from "../vendor/sortable"
 
@@ -51,10 +51,10 @@ Hooks.Sortable = {
 };
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks});
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks });
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"});
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300));
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide());
 
