8c3e6d393e9e72feecf14fe901483cfb48f2fd09
Tim Jarratt <tim.jarratt@maersk.com>
2026-03-06 19:00:45 +0100

    Add credo file

---
+++ .credo.exs
@@ -0,0 +1,29 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: [
+          "lib/",
+          "test/"
+        ],
+        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
+      },
+      plugins: [],
+      requires: [],
+      strict: true,
+      parse_timeout: 5000,
+      color: true,
+      checks: %{
+        disabled: [
+          # The following checks are disabled because they produce too many issues for
+          # the existing codebase. Consider enabling them incrementally.
+          {Credo.Check.Design.DuplicatedCode, []},
+          {Credo.Check.Readability.ModuleDoc, []},
+          {Credo.Check.Refactor.ABCSize, []},
+          {Credo.Check.Refactor.ModuleDependencies, []}
+        ]
+      }
+    }
+  ]
+}
